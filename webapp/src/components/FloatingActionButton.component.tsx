cf544e180d0ac61d87ff8bb8804ea0717507df63
Priyank Patel <patel23priyank@gmail.com>
2026-01-17 13:50:14 +0000

    set size of FloatingActionButton to md

--- webapp/src/components/FloatingActionButton.component.tsx
+++ webapp/src/components/FloatingActionButton.component.tsx
@@ -11,7 +11,7 @@ const FloatingActionButton = ({
 }) => {
   return (
     <Affix position={{ bottom: 16, right: 16 }} zIndex={100}>
-      <Button hiddenFrom="sm" radius="xl" className="fab-shadow" onClick={onClick}>
+      <Button hiddenFrom="sm" radius="xl" size="md" className="fab-shadow" onClick={onClick}>
         <Icon size={20} stroke={1.5} />
       </Button>
 
@@ -19,6 +19,7 @@ const FloatingActionButton = ({
         visibleFrom="sm"
         leftSection={<Icon size={20} stroke={1.5} />}
         radius="xl"
+        size="md"
         className="fab-shadow"
         onClick={onClick}
       >
